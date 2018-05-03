3fcf8ba8e55f3e2a81cad8ef9a221dfd1c1b4376
taramouse <nickykeyse@gmail.com>
2018-05-03 15:15:50 +0100

    Added and displayed vuex store items.

--- src/store.js
+++ src/store.js
@@ -5,12 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    msg: 'This is a store message.',
+    data: [
+      10, 202, 30, 69
+    ],
+    items: [
+      { name: 'Laps', value: 3 },
+      { name: 'Circuit', value: 'Silverstone' },
+      { name: 'Model', value: 'DB9' },
+      { name: 'Manufacturer', value: 'Aston Martin' },
+      { name: 'Best Lap', value: '1:43:23' }
+    ]
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    getMessage (state) {
+      return state.msg
+    },
+    getData (state) {
+      return state.data
+    },
+    getItems (state) {
+      return state.items
+    }
   }
 })
